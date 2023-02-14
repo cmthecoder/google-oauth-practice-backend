d762e839d7f1f7f9816c12890e0d7a1452bfb5f1
Chris Mems <christopher.mems@.com>
2023-02-14 11:33:56 -0600

    import passport into server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
+const passportSetup = require('./passport');
 const app = express();
 
 app.use(
